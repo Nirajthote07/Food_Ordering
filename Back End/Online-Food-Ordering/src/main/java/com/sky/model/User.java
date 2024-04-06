9bd0d1fb5a551009974e93df918354975aa87eb4
Niraj Thote <nirajshivamthote20@gmail.com>
2024-04-06 14:19:47 +0530

    First commit made somr model

---
+++ Back End/Online-Food-Ordering/src/main/java/com/sky/model/User.java
@@ -0,0 +1,37 @@
+package com.sky.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sky.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+
+    private List<RestaurantDto> favorites = new ArrayList<>();
+}
