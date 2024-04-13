a190589dd3b04332c62b5f5b5f8cc1c03a5eafe0
Niraj Thote <nirajshivamthote20@gmail.com>
2024-04-14 01:05:07 +0530

    Cart service api done

---
+++ Back End/Online-Food-Ordering/src/main/java/com/sky/service/CartServiceImp.java
@@ -0,0 +1,131 @@
+package com.sky.service;
+
+import com.sky.model.Cart;
+import com.sky.model.CartItem;
+import com.sky.model.Food;
+import com.sky.model.User;
+import com.sky.repository.CartItemRepository;
+import com.sky.repository.CartRepository;
+import com.sky.repository.FoodRepository;
+import com.sky.request.AddCartItemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class CartServiceImp implements CartService{
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private FoodService foodService;
+
+
+
+    @Override
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        Food food = foodService.findFoodById(req.getFoodId());
+        Cart cart = cartRepository.findByCustomerId(user.getId());
+
+        for (CartItem cartItem : cart.getItem()){
+            if(cartItem.getFood().equals(food)){
+                int newQuantity = cartItem.getQuantity()+ req.getQuantity();
+
+                return updateCartItemQuantity(cartItem.getId(),newQuantity);
+            }
+        }
+
+        CartItem newCartItem = new CartItem();
+        newCartItem.setFood(food);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(req.getQuantity());
+        newCartItem.setIngredients(req.getIngredients());
+        newCartItem.setTotalPrice(req.getFoodId()*food.getPrice());
+
+        CartItem savedCartItem = cartItemRepository.save(newCartItem);
+        cart.getItem().add(savedCartItem);
+
+        return savedCartItem;
+    }
+
+    @Override
+    public CartItem updateCartItemQuantity(Long cartItemId, int quantity) throws Exception {
+
+        Optional<CartItem> cartItemOptional = cartItemRepository.findById(cartItemId);
+        if(cartItem.isEmpty())
+            throw new Exception("CartItem not Found");
+
+        CartItem cartItem = cartItemOptional.get();
+        cartItem.setQuantity(quantity);
+        cartItem.setTotalPrice(cartItem.getFood().getPrice()*quantity);
+
+        return cartItemRepository.save(cartItem);
+    }
+
+    @Override
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Cart cart = cartRepository.findByCustomerId(user.getId());
+
+        Optional<CartItem> cartItemOptional = cartItemRepository.findById(cartItemId);
+        if (cartItemOptional.isEmpty())
+            throw new Exception("Cart Item not found");
+
+        CartItem cartItem = cartItemOptional.get();
+
+        cart.getItem().remove(cartItem);
+
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public Long calculateCartTotals(Cart cart) throws Exception {
+
+        Long total = 0L;
+
+        for(CartItem cartItem : cart.getItem()){
+            total += cartItem.getTotalPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public Cart findCartById(Long id) throws Exception {
+
+        Optional<Cart> optionalCart = cartRepository.findById(id);
+        if (optionalCart.isEmpty())
+            throw new Exception("Cart not found with id"+id);
+
+        return optionalCart.get();
+    }
+
+    @Override
+    public Cart findCartByUserId(String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        return cartRepository.findByCustomerId(user.getId());
+    }
+
+    @Override
+    public Cart clearCart(String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = findCartByUserId(jwt);
+
+        cart.getItem().clear();
+        cart.setTotal(0L);
+
+        return cartRepository.save(cart);
+    }
+}
